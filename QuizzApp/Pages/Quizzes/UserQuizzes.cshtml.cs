3dea7b183cf0ab03dbc5915138e492688be24a7e
j-osypiuk <kuba6535@gmail.com>
2023-04-30 13:45:37 +0200

    User quizzes page

--- QuizzApp/Pages/Quizzes/UserQuizzes.cshtml.cs
+++ QuizzApp/Pages/Quizzes/UserQuizzes.cshtml.cs
@@ -1,12 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using QuizzApp.Data;
+using QuizzApp.Model;
+using System.Security.Claims;
 
 namespace QuizzApp.Pages.Quizzes
 {
     public class UserQuizzesModel : PageModel
     {
+        public ApplicationDbContext _db { get; set; }
+
+        [BindProperty]
+        public List<Quiz> UserQuizzes { get; set; }
+
+        public UserQuizzesModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
+            UserQuizzes = _db.Quiz.Where(q => q.UserId == int.Parse(HttpContext.User.FindFirstValue("UserId"))).ToList();
         }
     }
 }
