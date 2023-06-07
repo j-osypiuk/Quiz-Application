bf196d1172ecbb8701cf9c71ef3a7d69ab4aa6a3
j-osypiuk <kuba6535@gmail.com>
2023-06-07 13:01:08 +0200

    Registration for a quiz form

--- QuizzApp/Pages/Index.cshtml.cs
+++ QuizzApp/Pages/Index.cshtml.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using QuizzApp.Data;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace QuizzApp.Pages
 {
@@ -8,14 +12,53 @@ namespace QuizzApp.Pages
 	{
 		private readonly ILogger<IndexModel> _logger;
 
-		public IndexModel(ILogger<IndexModel> logger)
+		private readonly ApplicationDbContext _db;
+
+		[BindProperty]
+        [DisplayName("Quiz code")]
+        [Required]
+        public string QuizCode { get; set; }
+        
+        [BindProperty]
+        [DisplayName("First name")]
+        [Required]
+		public string FirstName { get; set; }
+        
+        [BindProperty]
+        [DisplayName("Last name")]
+        [Required]
+        public string LastName { get; set; }
+        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext db)
 		{
 			_logger = logger;
+			_db = db;
 		}
 
 		public void OnGet()
 		{
 
 		}
+
+		public async Task<IActionResult> OnPostAsync()
+		{	
+			if (ModelState.IsValid)
+			{
+				if(int.TryParse(QuizCode, out int number))
+				{
+                    bool exists = await _db.Quiz.AnyAsync(q => q.QuizCode == int.Parse(QuizCode));
+                    if (!exists)
+                    {
+                        ModelState.AddModelError("QuizCode", "Quiz with this quiz code doesn't exist.");
+                        return Page();
+                    }
+                } else
+				{
+                    ModelState.AddModelError("QuizCode", "Quiz code must contain only numbers.");
+                    return Page();
+                }
+				return RedirectToPage("/Quizzes/SolveQuiz", new { FirstName, LastName, QuizCode });
+			}
+            return Page();
+        }
 	}
 }
--- QuizzApp/Pages/Quizzes/SolveQuiz.cshtml.cs
+++ QuizzApp/Pages/Quizzes/SolveQuiz.cshtml.cs
@@ -9,9 +9,12 @@ namespace QuizzApp.Pages.Quizzes
 	public class SolveQuizModel : PageModel
     {
         private readonly ApplicationDbContext _db;
-		
+
 		[BindProperty]
-		public int QuizId { get; set; }
+		public string FirstName { get; set; }
+
+		[BindProperty]
+		public string LastName { get; set; }
 
 		[BindProperty]
 		public Quiz Quiz { get; set; }
@@ -24,30 +27,44 @@ namespace QuizzApp.Pages.Quizzes
 			_db = db;
 		}
 
-		public async Task<IActionResult> OnGetAsync(int quizId)
+		public async Task<IActionResult> OnGetAsync(string? firstName, string? lastName, int? quizCode)
         {
-			QuizId = 26;
-			Quiz = await _db.Quiz.Include(q => q.Questions).ThenInclude(q => q.Answers).SingleOrDefaultAsync(q => q.Id == QuizId);
+			Quiz = await _db.Quiz.Include(q => q.Questions).ThenInclude(q => q.Answers).SingleOrDefaultAsync(q => q.QuizCode == quizCode);
 
+			if (Quiz != null)
+			{
+				FirstName = firstName;
+				LastName = lastName;
+			}
 
 			return Page();
         }
 
         public async Task<IActionResult> OnPostAsync()
         {	
-			var questions = _db.Question.Include(q => q.Answers).Where(q => q.QuizId == Quiz.Id).ToList();
+			Quiz = await _db.Quiz.Include(q => q.Questions).ThenInclude(q => q.Answers).SingleOrDefaultAsync(q => q.Id == Quiz.Id);
 
 			int correctAnswers = 0;
 			int j = 0;
-			foreach (var question in questions) 
+			foreach (var question in Quiz.Questions) 
 			{
 				for (int i = 0; i < question.Answers.Count; i++, j++)
 				{
 					if (question.Answers[i].IsCorrect == IsAnswerCorrect[j]) correctAnswers++;
 				}
 			}
+			double score = (double)correctAnswers / (double)IsAnswerCorrect.Count * 100;
 
-			double score = (double)correctAnswers / (double)IsAnswerCorrect.Count;
+			_db.Result.Add(new Result
+			{
+				FirstName = FirstName,
+				LastName = LastName,
+				Score = (int)Math.Round(score),
+				CreateDate = DateTime.Now,
+				Quiz = Quiz
+			});
+
+			await _db.SaveChangesAsync();
 
             return RedirectToPage("/Index");
         }
