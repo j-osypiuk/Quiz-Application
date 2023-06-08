1d45962c3caee322f599be418a30b37ecc60aadc
j-osypiuk <kuba6535@gmail.com>
2023-06-08 21:07:00 +0200

    Delete quiz result implementation

--- QuizzApp/Pages/Quizzes/QuizResults.cshtml.cs
+++ QuizzApp/Pages/Quizzes/QuizResults.cshtml.cs
@@ -10,6 +10,12 @@ namespace QuizzApp.Pages.Quizzes
         private readonly ApplicationDbContext _db;
 
         [BindProperty]
+        public int QuizId { get; set; }
+
+        [BindProperty]
+        public List<bool> ToDelete { get; set; } = new List<bool>();
+
+        [BindProperty]
         public List<Result> Results { get; set; }
 
         [BindProperty]
@@ -22,8 +28,28 @@ namespace QuizzApp.Pages.Quizzes
 
         public void OnGet(int quizId, string title)
         {
-            Results = _db.Result.Where(r => r.QuizId == quizId).ToList();
+            QuizId = quizId;
+            Results = _db.Result.Where(r => r.QuizId == QuizId).ToList();
             Title = title;
+            foreach (var result in Results)
+            {
+                ToDelete.Add(false);
+            }
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var results = _db.Result.Where(r => r.QuizId == QuizId).ToList();
+
+            for (int i = 0; i < results.Count; i++)
+            {
+                if (ToDelete[i])
+                {
+                    _db.Result.Remove(results[i]);
+                }
+            }
+            await _db.SaveChangesAsync();
+            return RedirectToPage("/Quizzes/QuizResults", new {QuizId, Title});
         }
     }
 }
